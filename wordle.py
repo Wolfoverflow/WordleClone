743184f47bf026def32e064a0183acaebb402136
Wolfoverflow <57900648+Wolfoverflow@users.noreply.github.com>
2023-04-22 22:42:54 +1000

    Fixed double ups
    Made a dict of all characters that aren’t in correct with the key as the char and the value as the amount of times the character appears in the word
    
    TL;DR Used a dict as a counter to determine amount of times to do yellow

--- wordle.py
+++ wordle.py
@@ -1,31 +1,54 @@
 from termcolor import colored
 from random import choice
+import wordle_words
 
-with open('words.txt') as f:
-    words = f.read()
+words = wordle_words.words
 def correct(letter):
     return colored(letter, 'white', 'on_green', attrs=['bold'])
 def present(letter):
     return colored(letter, 'white', 'on_yellow', attrs=['bold'])
 def absent(letter):
     return colored(letter, 'white', 'on_grey', attrs=['bold'])
+
 def check(word, guess):
-    for word_character, guess_character in zip(word, guess):
-        if word_character == guess_character:
-            print(correct(word_character), end='')
-        elif guess_character in word and word_character != guess_character:
-            print(present(guess_character), end='')
+    output = []
+    counter = {}
+
+    for i in word:
+        counter[i] = 0
+    for i in word:
+        counter[i] += 1
+
+    for iter, i in enumerate(guess):
+        if i == word[iter]:
+            counter[i] -= 1
+
+    for iter, i in enumerate(guess):
+        if i == word[iter]:
+            output.append(correct(i))
+        elif i in word:
+            if counter[i] != 0:
+                output.append(present(i))
+                counter[i] -= 1
+            else:
+                output.append(absent(i))
         else:
-            print(absent(guess_character), end='')
+            output.append(absent(i))
+
+    print("\r\r" + ''.join(output) + "                     ")
+
 
 def main():
     word = choice(words)
+    print("Best of luck!")
     for i in range(6):
-        guess = input()
+        guess = input("_____\r")
         while guess not in words:
-            guess = input()
+            print("\rInvalid guess. Try again.")
+            guess = input("_____\r")
         check(word.upper(), guess.upper())
         if guess == word:
+            print(correct(guess.upper()))
             print(f"You won with {5-i} guesses left!")
             break
     else:
